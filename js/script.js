2b72478041a4e61c373085e2c349d2cdb5c7bcdf
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-23 15:35:34 +0100

    add with append variable myNumber to DOM

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+const sectionElement= document.querySelector("section")
+
 for(let i = 1; i <= 100; i ++){
-  // console.log(i);
+  let myNumber;
   if ((i % 3 === 0) && (i % 5 === 0)){
     console.log("FizzBuzz");
+    myNumber= "FizzBuzz";
   }else if (i % 3 === 0){
     console.log("Fizz");
+    myNumber= "Fizz";
   }else if (i % 5 === 0){
     console.log("Buzz");
+    myNumber= "Buzz"
   }else{
-    console.log(i)
+    console.log(i); 
+    myNumber = i
   }
+
+  sectionElement.append(myNumber) 
 }
+
+
