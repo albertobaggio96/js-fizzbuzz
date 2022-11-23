d2c60c8f5a53ba0f05ca63b792bf5932a53cae77
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-23 15:54:47 +0100

    applied ; where it was necessary

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-const sectionElement= document.querySelector("section")
+const sectionElement = document.querySelector("section");
 
 for(let i = 1; i <= 100; i ++){
   let myNumber;
   if ((i % 3 === 0) && (i % 5 === 0)){
     console.log("FizzBuzz");
-    myNumber= "FizzBuzz";
+    myNumber = "FizzBuzz";
   }else if (i % 3 === 0){
     console.log("Fizz");
-    myNumber= "Fizz";
+    myNumber = "Fizz";
   }else if (i % 5 === 0){
     console.log("Buzz");
-    myNumber= "Buzz"
+    myNumber = "Buzz";
   }else{
     console.log(i); 
-    myNumber = i
+    myNumber = i;
   }
 
   // sectionElement.innerHTML+= `<div>${myNumber}</div>`
 
-  if (myNumber=== "FizzBuzz"){
-    sectionElement.innerHTML+= `<div class="red">${myNumber}</div>`
+  if (myNumber === "FizzBuzz"){
+    sectionElement.innerHTML += `<div class="red">${myNumber}</div>`;
   } else if (myNumber === "Fizz"){
-    sectionElement.innerHTML+= `<div class="blue">${myNumber}</div>`
+    sectionElement.innerHTML += `<div class="blue">${myNumber}</div>`;
   } else if (myNumber === "Buzz"){
-    sectionElement.innerHTML+= `<div class="green">${myNumber}</div>`
+    sectionElement.innerHTML += `<div class="green">${myNumber}</div>`;
   } else {
-    sectionElement.innerHTML+= `<div>${myNumber}</div>`
+    sectionElement.innerHTML += `<div>${myNumber}</div>`;
   }
 }
 
